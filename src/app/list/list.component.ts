04224a4c82b866f12794955b8c06abc3c6c6dffe
Volodymyr Umrysh <umrysh.v@gmail.com>
2019-11-26 20:54:33 +0200

    26/3.11

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -5,9 +5,9 @@ import { FormGroup } from '@angular/forms';
 
 export interface List{
   checked: boolean;
-  date: string
-  title: string
-  sum: number
+  date: string;
+  title: string;
+  sum: number;
 }
 @Component({
   selector: 'app-list',
@@ -19,20 +19,42 @@ export class ListComponent implements OnInit {
   @ViewChild("closeWindow", {static:false}) closeWindow:ElementRef;
   form: FormGroup;
   search:'';
- 
+  s:number=0;
+  sumIncome: number = 0;
+  sumExpance: number = 0;
+  array;
+
   
   constructor(private http: HttpClient, public balance:BalanceService){
 console.log(this.balance)
   }
-  
 
   ngOnInit() {
-   
     this.balance.getList()
+    
+
+    setTimeout(() => {
+      this.array = this.balance.list;
+      this.array.forEach(element => {
+  
+        if(element.type === 'income'){
+  this.s += Number(element.sum);
+
+  this.sumIncome += Number(element.sum);
+        }else{
+          this.s = this.s - Number(element.sum);
+          this.sumExpance += Number(element.sum);
+        }
+      });
+    }, 5000);
+
+
+    console.log(this.sumExpance);
+    
   }
 
   closeModal(){
-    this.closeWindow.nativeElement.click()
+    this.closeWindow.nativeElement.click()  
 }
 
 }
